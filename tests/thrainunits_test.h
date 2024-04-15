7ad881d8ab0997889ade1d00708f55fcf49bbefe
rboston628@gmail.com <grpulse@GRPulse.local>
2024-04-15 00:50:29 -0400

    try fixing test 5

--- tests/thrainunits_test.h
+++ tests/thrainunits_test.h
@@ -13,13 +13,13 @@ public:
         delete suite; 
     }
 
-    void setUp() {
-        freopen("tests/artifacts/logio.txt", "a", stdout);
-    }
+    // void setUp() {
+    //     freopen("tests/artifacts/logio.txt", "a", stdout);
+    // }
 
-    void tearDown() {
-        freopen("/dev/tty", "w", stdout);
-    }
+    // void tearDown() {
+    //     freopen("/dev/tty", "w", stdout);
+    // }
 
     Calculation::OutputData setupFakeCalcData(units::Units unitType){
         Calculation::OutputData fakeData;
